a945e17c7036b4824d6933b05e4d89c28c1bd77c
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-09-21 21:47:32 +0530

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 name = st.text_input("Enter your Name : ")
 fname = st.text_input("Enter Your surname: ")
 adr = st.text_area("Enter your text: ")
-classdata = st.selectbox("Enter your class : ",(1,2,3,4,5,6,7))
+classdata = st.selectbox("Skills : ",["Software", "Hardware", "HTML", "CSS", "JavaScript", "Java", "Mobile"])
 
 button = st.button("Done")
 if button :
