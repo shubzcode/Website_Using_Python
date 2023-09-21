cff767d7d1890084a4fc6243055e5ce4fa55a49d
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-09-21 22:24:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Hello Techeie !!!")
 name = st.text_input("Enter your Name : ")
 fname = st.text_input("Enter Your surname: ")
-adr = st.text_area("Enter your text: ")
+adr = st.text_area("Address: ")
 status = st.radio("Select Gender: ", ('Male', 'Female'))
 if (status == 'Male'):
     st.success("Male")
