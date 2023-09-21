32eed543b80efaf7685f7cc7c4b24b81df37102a
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-09-21 22:59:38 +0530

    p;

--- main.py
+++ main.py
@@ -1,20 +1,61 @@
-import streamlit as st 
+import streamlit as st
+import streamlit.components.v1 as components
 
-st.title("Hello Techeie !!!")
-name = st.text_input("Enter your Name : ")
-fname = st.text_input("Enter Your surname: ")
-adr = st.text_area("Address: ")
-status = st.radio("Select Gender: ", ('Male', 'Female'))
-if (status == 'Male'):
-    st.success("Male")
-else:
-    st.success("Female")
-classdata = st.selectbox("Skills : ",["Software", "Hardware", "HTML", "CSS", "JavaScript", "Java", "Mobile"])
+# bootstrap 4 collapse example
+components.html(
+    """
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <div id="accordion">
+      <div class="card">
+        <div class="card-header" id="headingOne">
+          <h5 class="mb-0">
+            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            Collapsible Group Item #1
+            </button>
+          </h5>
+        </div>
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+          <div class="card-body">
+            Collapsible Group Item #1 content
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header" id="headingTwo">
+          <h5 class="mb-0">
+            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            Collapsible Group Item #2
+            </button>
+          </h5>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+          <div class="card-body">
+            Collapsible Group Item #2 content
+          </div>
+        </div>
+      </div>
+    </div>
+    """,
+    height=600,
+)
 
-button = st.button("Done")
-if button :
-    st.markdown(f"""
-                Name : {name}
-                Surname : {fname}
-                address : {adr}
-                class : {classdata}""")
+# st.title("Hello Techeie !!!")
+# name = st.text_input("Enter your Name : ")
+# fname = st.text_input("Enter Your surname: ")
+# adr = st.text_area("Address: ")
+# status = st.radio("Select Gender: ", ('Male', 'Female'))
+# if (status == 'Male'):
+#     st.success("Male")
+# else:
+#     st.success("Female")
+# classdata = st.selectbox("Skills : ",["Software", "Hardware", "HTML", "CSS", "JavaScript", "Java", "Mobile"])
+
+# button = st.button("Done")
+# if button :
+#     st.markdown(f"""
+#                 Name : {name}
+#                 Surname : {fname}
+#                 address : {adr}
+#                 class : {classdata}""")
