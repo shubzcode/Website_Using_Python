1730c34e146555b4239e20ae21b53fc50e0e6073
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-09-21 22:23:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ st.title("Hello Techeie !!!")
 name = st.text_input("Enter your Name : ")
 fname = st.text_input("Enter Your surname: ")
 adr = st.text_area("Enter your text: ")
+status = st.radio("Select Gender: ", ('Male', 'Female'))
+if (status == 'Male'):
+    st.success("Male")
+else:
+    st.success("Female")
 classdata = st.selectbox("Skills : ",["Software", "Hardware", "HTML", "CSS", "JavaScript", "Java", "Mobile"])
 
 button = st.button("Done")
